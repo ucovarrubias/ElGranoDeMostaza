49f896646ba36431fbaca068669089d194cf9606
Utiak <utiak.covarrubias@potros.itson.edu.mx>
2022-05-03 05:51:06 -0700

    Clase para generación de folios de 6 caracteres alfanuméricos

---
+++ src/java/util/GenerarFolio.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.util.Random;
+
+/**
+ *
+ * @author ucova
+ */
+public class GenerarFolio {
+    public String getCadenaAlfanumAleatoria(){
+        String cadenaAleatoria = "";
+        long milis = new java.util.GregorianCalendar().getTimeInMillis();
+        Random r = new Random(milis);
+        int i = 0;
+        while (i < 6){
+            char c = (char)r.nextInt(255);
+            if ( (c >= '0' && c <='9') || (c >='A' && c <='Z') ){
+                cadenaAleatoria += c;
+                i++;
+            }
+        }
+        return cadenaAleatoria;
+    }
+}
